9f8917632b4162a7efad04ebf172ff18f469c97a
Liam Colburn <colburnw@oregonstate.edu>
2023-10-16 11:07:30 -0700

    Abstract into get_course_list method

--- main.rb
+++ main.rb
@@ -1,14 +1,17 @@
 class Student
   MAX_COURSES = 5
-  def remove_from_schedule(course, schedule, quarter_id)
+
+  def get_course_list(schedule, quarter_id)
     quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
+    quarter.course_list
+  end
+  def remove_from_schedule(course, schedule, quarter_id)
+    course_list = get_course_list(schedule, quarter_id)
     course_list.remove(course)
   end
 
   def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
+    course_list = get_course_list(schedule, quarter_id)
     max_courses = course_list.maximum_number_of_courses
     if max_courses < MAX_COURSES
       course_list.add(course)
